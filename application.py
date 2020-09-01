4cdfbe312464da8297c6e4363abf9faea1a87b8b
Piyush Sharma <45245975+piyushsharma220699@users.noreply.github.com>
2020-09-02 02:28:21 +0530

    Updated Application.py

--- application.py
+++ application.py
@@ -6,21 +6,32 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://web.whatsapp.com/")
 driver.maximize_window()
 
-text= "Dear Meow, Ishu is sleeping right now! He will reply to your texts when he gets up. BTW this is an auto generated reply from his bot. Thanks!"
+# Texts which you want to send
+text= "Dear "
+text2=", He is sleeping right now! He will reply to your texts when he gets up. BTW this is an auto generated reply from his bot. Thanks!"
+
 # Time provided to scan the QR code of Whatsapp Web
 time.sleep(10)
 
-# Click on the Chat
-user=driver.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[17]/div/div/div[2]/div[1]/div[1]/span/span')
-user.click()
+# List of names to which you want to send the message
+namelist = ["Meow","Mom"]
 
-# Type the message on the Chatbox
-textbox=driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-textbox.send_keys(text)
+for name in namelist:
+    # Click on the Chat
+    user=driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
 
-# Send Message
-send=driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')
-send.click()
+    # Type the message on the Chatbox
+    textbox=driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+    textbox.send_keys(text)
+    textbox.send_keys(name)
+    textbox.send_keys(text2)
 
-time.sleep(5)
+    # Send Message
+    send=driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')
+    send.click()
+
+    time.sleep(5)
+
 driver.quit()
+# unreadMsgs = browser.find_elements_by_xpath(".//span[span[@class='unread-count icon-meta'] and not(span[@class='icon icon-meta icon-muted'])]")
